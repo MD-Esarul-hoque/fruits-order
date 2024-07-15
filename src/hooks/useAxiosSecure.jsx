8f7b7ff0defb15135936389bda8685cead7f7518
Esarue <esarulhoqueweb@gmail.com>
2024-07-15 14:00:13 +0600

    last commi3ee3t

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
  const axiosSecure=axios.create({
-    baseURL:'https://fruits-server-lnshx3gf1-mdesarulhoques-projects.vercel.app'
+    baseURL:'https://fruits-server.vercel.app'
 })
 const useAxiosSecure = () => {
     return axiosSecure;
--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -13,7 +13,7 @@ import { useEffect, useState } from "react";
 const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
     useEffect(() => {
-        fetch('https://fruits-server-lnshx3gf1-mdesarulhoques-projects.vercel.app/reviews')
+        fetch('https://fruits-server.vercel.app/reviews')
             .then(res => res.json())
             .then(data => setReviews(data))
 
