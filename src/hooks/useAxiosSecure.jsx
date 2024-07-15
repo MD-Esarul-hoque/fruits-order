f7f3ba4987e101d300be4258d6686e113d8d5b8b
Esarue <esarulhoqueweb@gmail.com>
2024-07-15 10:46:45 +0600

    fruissot

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
  const axiosSecure=axios.create({
-    baseURL:'http://localhost:5016https://fruits-server.vercel.app/'
+    baseURL:'https://fruits-server.vercel.app/'
 })
 const useAxiosSecure = () => {
     return axiosSecure;
